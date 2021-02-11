e4f3f5760ce3fa26e46fdb2e0e5370ce06fafa44
Santiago Geliz <gelizsantiago93@gmail.com>
2021-02-10 19:38:51 -0500

    Modificación en DB

--- secoswebpage/settings/production.py
+++ secoswebpage/settings/production.py
@@ -17,8 +17,6 @@ DATABASES = {
         'NAME': config('DB_NAME'),
         'USER': config('DB_USER'),
         'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST'),
-        'PORT': '5432',
     }
 }
 
