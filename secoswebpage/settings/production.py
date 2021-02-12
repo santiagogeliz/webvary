7ac98eb1adb8d528da2289660b864ff8c17eb6bb
Santiago Geliz <gelizsantiago93@gmail.com>
2021-02-12 00:04:57 -0500

    Configuración Final Heroku

--- secoswebpage/settings/production.py
+++ secoswebpage/settings/production.py
@@ -13,13 +13,18 @@ ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT'),
     }
 }
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
+
 
 # EMAIL
 # ------------------------------------------------------------------------------
