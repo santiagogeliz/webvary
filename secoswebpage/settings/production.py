f28be6f1e9e2ef449a6237c287b19dad18f0a5c4
Santiago Geliz <gelizsantiago93@gmail.com>
2021-02-11 20:27:22 -0500

    Configuración Final Heroku

--- secoswebpage/settings/production.py
+++ secoswebpage/settings/production.py
@@ -17,6 +17,8 @@ DATABASES = {
         'NAME': config('DB_NAME'),
         'USER': config('DB_USER'),
         'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT'),
     }
 }
 
